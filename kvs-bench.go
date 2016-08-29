947f11fd4e9079fe310deb7298b7f72829c5cfa8
Alexander Sosna <alexander@xxor.de>
2016-08-29 11:43:26 +0200

    At logrus

--- kvs-bench.go
+++ kvs-bench.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/xxorde/kvs"
 	"math/rand"
 	"os"
 	"strconv"
 	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/xxorde/kvs"
 )
 
 func main() {
@@ -17,12 +18,12 @@ func main() {
 	var seed int64
 	var dumpfile string
 	var backup bool
-	line := "==================================================================="
+	const line = "==================================================================="
 	flag.Int64Var(&nKeys, "keys", 1000000, "Number of key / value pairs")
 	flag.Int64Var(&nRndWrites, "writes", 1000000, "Number of random writes")
 	flag.Int64Var(&nRndReads, "reads", 1000000, "Number of random reads")
 	flag.Int64Var(&seed, "seed", 1337, "seed for rand")
-	flag.BoolVar(&backup, "backup", false, "should backup be performed?")
+	flag.BoolVar(&backup, "backup", true, "should backup be performed?")
 	flag.StringVar(&dumpfile, "of", "kvs.dump", "output file for the dump")
 	flag.Parse()
 
@@ -34,70 +35,70 @@ func main() {
 
 	// create n key / values
 	start := time.Now()
-	fmt.Println(line)
-	fmt.Printf("Create %d key / value pairs\n", nKeys)
+	log.Info(line)
+	log.Infof("Create %d key / value pairs", nKeys)
 	for i := int64(0); i < nKeys; i++ {
 		go store.Put("key"+strconv.FormatInt(i, 10), "value"+strconv.FormatInt(i, 10))
 		if i < i-1 {
-			fmt.Println("Overflow: ", i)
+			log.Info("Overflow: ", i)
 		}
 	}
-	fmt.Println("Time: ", time.Since(start),
+	log.Info("Time: ", time.Since(start),
 		"time per key: ", time.Since(start).Nanoseconds()/int64(nKeys), "ns")
 
 	// write random value
 	start = time.Now()
-	fmt.Println(line)
-	fmt.Printf("Do %d random writes\n", nRndWrites)
+	log.Info(line)
+	log.Infof("Do %d random writes", nRndWrites)
 	for i := int64(0); i < nRndWrites; i++ {
 		store.Put("key"+strconv.FormatInt(rand.Int63n(nKeys), 10), "random write"+strconv.FormatInt(i, 10))
 	}
-	fmt.Println("Time: ", time.Since(start),
+	log.Info("Time: ", time.Since(start),
 		"time per write: ", time.Since(start).Nanoseconds()/int64(nRndWrites), "ns")
 
 	// read random value
 	start = time.Now()
-	fmt.Println(line)
-	fmt.Printf("Do %d random reads\n", nRndReads)
+	log.Info(line)
+	log.Infof("Do %d random reads", nRndReads)
 	tmp := ""
 	for i := int64(0); i < nRndReads; i++ {
 		tmp = store.Get("key" + strconv.FormatInt(rand.Int63n(nKeys), 10))
 	}
 	// use it or get en error :)
 	//tmp = tmp
-	fmt.Println("Last read value:", tmp)
-	fmt.Println("Time: ", time.Since(start),
+	log.Info("Last read value:", tmp)
+	log.Info("Time: ", time.Since(start),
 		"time per read: ", time.Since(start).Nanoseconds()/int64(nRndReads), "ns")
 
 	// check random tuple
 	start = time.Now()
-	fmt.Println(line)
-	fmt.Printf("Do %d random tuple checks\n", nRndReads)
+	log.Info(line)
+	log.Infof("Do %d random tuple checks", nRndReads)
 	for i := int64(0); i < nRndReads; i++ {
 		store.Exists("key" + strconv.FormatInt(rand.Int63n(nKeys), 10))
 	}
-	fmt.Println("Time: ", time.Since(start),
+	log.Info("Time: ", time.Since(start),
 		"time per check: ", time.Since(start).Nanoseconds()/int64(nRndReads), "ns")
 
 	// check random tuple
 	start = time.Now()
-	fmt.Println(line)
-	fmt.Printf("Do %d random tuple checks on NONEXISTING tuples\n", nRndReads)
+	log.Info(line)
+	log.Infof("Do %d random tuple checks on NONEXISTING tuples", nRndReads)
 	for i := int64(0); i < nRndReads; i++ {
 		store.Exists("NOTHERE" + strconv.FormatInt(rand.Int63n(nKeys), 10))
 	}
-	fmt.Println("Time: ", time.Since(start),
+	log.Info("Time: ", time.Since(start),
 		"time per check: ", time.Since(start).Nanoseconds()/int64(nRndReads), "ns")
 
 	// some info
-	fmt.Println(line)
-	fmt.Println("Size of kvs is: ", store.Len())
+	log.Info(line)
+	log.Info("Size of kvs is: ", store.Len())
 
 	if backup {
 		// write to file
 		start = time.Now()
-		fmt.Println(line)
-		fmt.Println("Write data to file")
+		log.Info(line)
+		log.Info("Write data to file")
 		f, err := os.Create(dumpfile)
 		if err != nil {
 			panic("cant open file")
@@ -105,17 +106,17 @@ func main() {
 		store.DumpYaml(f)
 		f.Sync()
 		f.Close()
-		fmt.Println("Time: ", time.Since(start),
+		log.Info("Time: ", time.Since(start),
 			"time per dump: ", time.Since(start).Nanoseconds()/int64(nKeys), "ns")
 
 		/*// compress and write to file
 		start = time.Now()
-		fmt.Println(line)
-		fmt.Println("Compress write data to file")
+		log.Info(line)
+		log.Info("Compress write data to file")
 		store.BackupBinGz(dumpfile+".gz")
-		fmt.Println("Time: ", time.Since(start),
+		log.Info("Time: ", time.Since(start),
 		"time per dump: ", time.Since(start).Nanoseconds()/int64(nKeys), "ns")*/
 	}
 
-	fmt.Println(line)
+	log.Info(line)
 }
